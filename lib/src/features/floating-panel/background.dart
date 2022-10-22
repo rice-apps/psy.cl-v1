45b90b2180baca5668dddbc5ffcf88cf338ebec2
Ovikx <ovikdas10@gmail.com>
2022-10-22 14:48:08 -0500

    remove unused import

--- lib/src/features/floating-panel/background.dart
+++ lib/src/features/floating-panel/background.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:psycl/main.dart';
 
 class BackgroundColorPage extends StatelessWidget {
   const BackgroundColorPage({super.key});
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    backgroundColor: Colors.red,
-    appBar: AppBar(
-      //title:Text(MyApp.title),
-      //centerTitle: true,
-    ),
-  );
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+            //title:Text(MyApp.title),
+            //centerTitle: true,
+            ),
+      );
 }
