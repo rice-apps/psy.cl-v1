6efe431f07df587647972c212a2814982a3897d2
Victor Xie <victorxie@Victors-MacBook-Air-2.local>
2022-10-22 23:55:53 -0500

    initial setup for aws backeend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "psycl": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
