1e7b6d128955fa74cc767c9e7bba1d8cb5a9572f
JahnaviMahajan <mahajancodex@gmail.com>
2022-10-29 15:02:23 -0500

    Added interactivity to progress circle

--- lib/progresscircle.dart
+++ lib/progresscircle.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:flutter_circular_slider/flutter_circular_slider.dart';
 
 class ProgressCircle extends StatefulWidget {
   const ProgressCircle({super.key});
@@ -13,14 +14,14 @@ class ProgressCircle extends StatefulWidget {
 class _ProgressCircleState extends State<ProgressCircle> {
   int _count = 14;
 
-  void _incrementCounter() {
+  void _incrementCounter(int init, int end, int laps) {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _count++;
+      _count = end;
     });
   }
 
@@ -28,27 +29,58 @@ class _ProgressCircleState extends State<ProgressCircle> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red.shade300,
-      body: Center(
-        child: CircularPercentIndicator(
-          radius: 300,
-          lineWidth: 20,
-          animation: true,
-          animationDuration: 800,
-          percent: (_count / 28),
-          progressColor: Colors.white,
-          backgroundColor: Colors.grey.shade300,
-          circularStrokeCap: CircularStrokeCap.round,
-          center: Text(
-            '${28 - _count} days \n until period starts',
-            style: const TextStyle(fontSize: 25, color: Colors.white),
-            textAlign: TextAlign.center,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          CircularPercentIndicator(
+            radius: 300,
+            lineWidth: 20,
+            animation: false,
+            animationDuration: 800,
+            percent: (_count / 28),
+            progressColor: Colors.white,
+            backgroundColor: Colors.grey.shade300,
+            circularStrokeCap: CircularStrokeCap.round,
+            center: Text(
+              '${28 - _count} days \n until period starts',
+              style: const TextStyle(fontSize: 25, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
           ),
-        ),
+          SingleCircularSlider(
+            28,
+            28 - _count,
+            height: 300,
+            width: 300,
+            onSelectionChange: _incrementCounter,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _incrementCounter,
-      ),
+
+      // Center(
+      //   child: CircularPercentIndicator(
+      //     radius: 300,
+      //     lineWidth: 20,
+      //     animation: true,
+      //     animationDuration: 800,
+      //     percent: (_count / 28),
+      //     center: SingleCircularSlider(28, 1),
+
+      //     progressColor: Colors.white,
+      //     backgroundColor: Colors.grey.shade300,
+      //     circularStrokeCap: CircularStrokeCap.round,
+      //     //center: Text(
+      //'${28 - _count} days \n until period starts',
+      //style: const TextStyle(fontSize: 25, color: Colors.white),
+      //textAlign: TextAlign.center,
+      //),
     );
+    //     ),
+    //     floatingActionButton: FloatingActionButton(
+    //       child: Icon(Icons.add),
+    //       onPressed: _incrementCounter,
+    //     ),
+    //   );
+    //}
   }
 }
