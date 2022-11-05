dde491fe2ae93ef00274c16555560974cc452699
Jerry Li <jerry.len123@gmail.com>
2022-11-05 14:15:35 -0500

    data entry buttons in progress

--- lib/main.dart
+++ lib/main.dart
@@ -6,110 +6,101 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonColumnOne =
+        _buildButtonRow(Colors.red, Icons.call, "Spotting", "Low", "Normal");
+    Widget buttonColumnTwo =
+        _buildButtonRow(Colors.blue, Icons.call, "Spotting", "Not", "Normal");
+    Widget buttonColumnThree =
+        _buildButtonRow(color, Icons.call, "Spotting", "Nott", "Normal");
+    Widget buttonColumnFour =
+        _buildButtonRow(color, Icons.call, "Spotting", "ntt", "Normal");
+    Widget customButton = Ink.image(
+      image: const AssetImage('src/spotting.png'),
+      height: 10,
+      width: 10,
+    );
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter layout demo',
+      home: Scaffold(
+          body: Column(
+        children: [customButton],
+      )),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Row _buildButtonRow(Color color, IconData icon, String labelOne,
+      String labelTwo, String labelThree) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      SizedBox(
+        height: 75,
+        width: 75,
+        child: ElevatedButton.icon(
+            icon: Icon(icon, size: 24.0),
+            label: Text(labelOne),
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(20),
+              backgroundColor: color,
+            )),
+      ),
+      SizedBox(
+        height: 75,
+        width: 75,
+        child: ElevatedButton.icon(
+            icon: Icon(Icons.bloodtype),
+            label: Text(labelTwo),
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(20),
+              backgroundColor: Colors.red,
+            )),
+      ),
+      SizedBox(
+        height: 75,
+        width: 75,
+        child: ElevatedButton.icon(
+            icon: Icon(Icons.bloodtype),
+            label: Text(labelThree),
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(20),
+              backgroundColor: Colors.red,
+            )),
+      )
+    ]);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Icon(icon, color: color),
+        Row(
+            //margin: const EdgeInsets.only(top: 8),
+            children: <Widget>[
+              Text(
+                label,
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: color,
+                ),
+              ),
+              ElevatedButton(
+                  child: const Icon(Icons.train),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(20),
+                  ))
+            ]),
+      ],
     );
   }
 }
