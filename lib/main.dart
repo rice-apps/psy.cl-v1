a8eef926f3fa4835e558edbd4b6791ba118b3b99
Jerry Li <jerry.len123@gmail.com>
2022-11-19 14:10:13 -0600

    updated button functionality + containers

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,114 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+Map<String, String> invertImage = {};
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
-    Widget buttonColumnOne =
-        _buildButtonRow(Colors.red, Icons.call, "Spotting", "Low", "Normal");
-    Widget buttonColumnTwo =
-        _buildButtonRow(Colors.blue, Icons.call, "Spotting", "Not", "Normal");
-    Widget buttonColumnThree =
-        _buildButtonRow(color, Icons.call, "Spotting", "Nott", "Normal");
-    Widget buttonColumnFour =
-        _buildButtonRow(color, Icons.call, "Spotting", "ntt", "Normal");
-    Widget customButton = Ink.image(
-      image: const AssetImage('src/spotting.png'),
-      height: 10,
-      width: 10,
-    );
+    Widget customRowOne = _buildCustomButtonRow(
+        'assets/images/low.png',
+        'assets/images/spotting.png',
+        'assets/images/normal.png',
+        'assets/images/normal.png',
+        "Bleeding");
+    Widget customRowTwo = _buildCustomButtonRow(
+        'assets/images/exhausted.png',
+        'assets/images/tired.png',
+        'assets/images/normalEnergy.png',
+        'assets/images/normal.png',
+        "Energy");
+    Widget customRowThree = _buildCustomButtonRow(
+        'assets/images/sensitive.png',
+        'assets/images/sad.png',
+        'assets/images/happy.png',
+        'assets/images/normal.png',
+        "Mood");
+
     return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-          body: Column(
-        children: [customButton],
-      )),
-    );
+        title: 'Flutter layout demo',
+        home: Container(
+            color: Color.fromARGB(255, 201, 90, 90),
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 615,
+                width: 600,
+                color: Colors.white,
+                child: Column(
+                    children: [customRowOne, customRowTwo, customRowThree]))));
+  }
+
+  Widget buildButton(String imageSrc) {
+    return Material(
+        color: Colors.blue,
+        elevation: 8,
+        //borderRadius: BorderRadius.circular(28),
+        shape: const CircleBorder(),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Container(
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              shape: BoxShape.circle,
+              border: Border.all(
+                  color: Color.fromARGB(255, 236, 139, 132), width: 3),
+              //borderRadius: BorderRadius.circular(10)
+            ),
+            child: InkWell(
+                onTap: () => {
+                      setState(() {
+                        invertImage['pressed'] = 'assets/images/sensitive.png';
+                      })
+                    },
+                child: Ink.image(
+                  image: AssetImage(imageSrc),
+                  height: 83,
+                  width: 83,
+                  fit: BoxFit.cover,
+                ))));
+  }
+
+  Container _buildCustomButtonRow(String imageSrc1, String imageSrc2,
+      String imageSrc3, String imageSrc4, String header) {
+    return Container(
+        height: 175,
+        margin: const EdgeInsets.only(top: 15, bottom: 15),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Color.fromARGB(135, 77, 76, 76),
+                blurRadius: 15.0,
+                offset: Offset(0.0, 0.75))
+          ],
+        ),
+        child: Column(children: [
+          const SizedBox(height: 15),
+          Material(
+              child: Text(header,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ))),
+          const SizedBox(height: 15),
+          Container(
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buildButton(imageSrc1),
+                      buildButton(imageSrc2),
+                      buildButton(imageSrc3),
+                      buildButton(imageSrc4)
+                    ],
+                  )))
+        ]));
   }
 
   Row _buildButtonRow(Color color, IconData icon, String labelOne,
@@ -102,5 +185,11 @@ class MyApp extends StatelessWidget {
             ]),
       ],
     );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
