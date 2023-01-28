42f9fe67cd1f3bf2ec31481c8a543eed4320972a
JahnaviMahajan <mahajancodex@gmail.com>
2023-01-28 14:01:07 -0600

    Added read and writing classes

--- lib/local_files.dart
+++ lib/local_files.dart
@@ -9,7 +9,7 @@ void main(List<String> args) {
   runApp(
     MaterialApp(
       title: "Reading and writing files",
-      home: ReadandWrite(storage: CounterStorage()),
+      home: ReadAndWrite(storage: CounterStorage()),
     ),
   );
 }
@@ -26,4 +26,75 @@ class CounterStorage {
     return File('$path/counter.txt');
   }
 
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
 
+      // Read the file
+      final contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
+  }
+}
+
+class ReadAndWrite extends StatefulWidget {
+  const ReadAndWrite({super.key, required this.storage});
+
+  final CounterStorage storage;
+
+  @override
+  State<ReadAndWrite> createState() => _ReadAndWriteState();
+}
+
+class _ReadAndWriteState extends State<ReadAndWrite> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readCounter().then((value) {
+      setState(() {
+        _counter = value;
+      });
+    });
+  }
+
+  Future<File> _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+
+    // Write the variable as a string to the file.
+    return widget.storage.writeCounter(_counter);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reading and Writing Files'),
+      ),
+      body: Center(
+        child: Text(
+          'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
