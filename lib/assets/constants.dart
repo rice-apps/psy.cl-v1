5977cf3982e92755060a4236125159da1c7f0701
Ovikx <ovikdas10@gmail.com>
2023-02-04 15:20:43 -0600

    reorganize constants

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Sliding panel
 const MIN_PANEL_HEIGHT = 0.55;
 const MAX_PANEL_HEIGHT = 0.9;
 
+// Colors
 const Color PERIOD = const Color(0xFFF06565);
 const Color OVULATION = const Color(0xFFC2A3DB);
 const Color LUTEAL = const Color(0xFF82C2AB);
@@ -11,3 +13,6 @@ const Color WHITE = const Color(0xFFFDFDFD);
 const Color GRAY = const Color(0xFFBFBFBF);
 const Color DARKGRAY = const Color(0xFF797979);
 const Color BLACK = const Color(0xFF49505A);
+
+// Progress Circle
+const double STROKE_WIDTH = 20;
