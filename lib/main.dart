f826da0a4aaf06817fe4ad792a53101b957e9b65
joy <jy92@rice.edu>
2023-02-04 14:41:41 -0600

    color constants

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Psy.cl',
+      home: const MyHomePage(title: 'Psy.cl Home Page'),
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -77,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
         boxShadow: const [
-          BoxShadow(blurRadius: 8.0, color: Color.fromRGBO(0, 0, 0, 0.5))
+          BoxShadow(blurRadius: 8.0, color: constants.DARKGRAY)
         ],
       ),
     );
--- lib/src/features/landing-page/background.dart
+++ lib/src/features/landing-page/background.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import '../../../assets/constants.dart' as constants;
+
 
 class BackgroundColorPage extends StatelessWidget {
   const BackgroundColorPage({super.key}); //const?
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.redAccent,
+        //backgroundColor: Colors.redAccent,
+        backgroundColor: constants.WHITE,
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.red,
-                Colors.pink,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
+            // gradient: LinearGradient(
+            //   colors: [
+            //     constants.PERIOD,
+            //     constants.OVULATION,
+            //   ],
+            //   begin: Alignment.topLeft,
+            //   end: Alignment.bottomRight,
+            // ),
           ),
         ),
       );
