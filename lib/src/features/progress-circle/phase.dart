c38595ec88fa170b837c24ceef06c79c42cf9eb8
Ovikx <ovikdas10@gmail.com>
2023-02-04 15:56:32 -0600

    add toRadians method to phase and color field

--- lib/src/features/progress-circle/phase.dart
+++ lib/src/features/progress-circle/phase.dart
@@ -1,6 +1,26 @@
+import 'dart:math' as math;
+import '../../../assets/constants.dart' as constants;
+
+import 'package:flutter/material.dart';
+
+/// Represents a section on the progress circle
 class Phase {
   int startDay;
   int endDay;
+  Color color;
+  Paint paint = Paint();
 
-  Phase(this.startDay, this.endDay);
+  Phase(this.startDay, this.endDay, this.color) {
+    paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = constants.STROKE_WIDTH;
+  }
+
+  List<double> toRadians() {
+    return [
+      2 * math.pi * (startDay / 28) - math.pi / 2,
+      2 * math.pi * (endDay / 28) - math.pi / 2
+    ];
+  }
 }
