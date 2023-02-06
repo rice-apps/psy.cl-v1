56c38d2fb23002de53eebfc3058e95e55b66870c
Ovikx <ovikdas10@gmail.com>
2023-02-06 11:22:14 -0600

    remove nested align

--- lib/src/features/progress-circle/progress_circle.dart
+++ lib/src/features/progress-circle/progress_circle.dart
@@ -14,53 +14,49 @@ class _ProgressCircle extends State<ProgressCircle> {
   Widget build(BuildContext context) {
     const size = Size(250, 250);
     const radiusFactor = 2 / 3;
-    return Stack(children: [
-      Align(
-          alignment: Alignment.center,
-          child: CustomPaint(
-            painter: SectionedCircle(radiusFactor: radiusFactor),
-            size: size,
-            isComplex: true,
-          )),
-      Align(
-          alignment: Alignment.center,
-          child: Container(
-            alignment: Alignment.center,
-            height: size.height,
-            width: size.width,
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-                color: constants.WHITE,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 100,
-                      color: Colors.grey.shade400,
-                      spreadRadius: -4)
-                ]),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "10 days until period starts",
-                  style: TextStyle(
-                      fontFamily: "Metropolis",
-                      fontSize: 30,
-                      color: Colors.grey.shade800),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text("ovulation phase",
-                    style: TextStyle(
-                        fontFamily: "Metropolis",
-                        fontSize: 15,
-                        color: Colors.purple.shade300))
-              ],
+    return Stack(alignment: Alignment.center, children: [
+      CustomPaint(
+        painter: SectionedCircle(radiusFactor: radiusFactor),
+        size: size,
+        isComplex: true,
+      ),
+      Container(
+        alignment: Alignment.center,
+        height: size.height,
+        width: size.width,
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: constants.WHITE,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 100,
+                  color: Colors.grey.shade400,
+                  spreadRadius: -4)
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "10 days until period starts",
+              style: TextStyle(
+                  fontFamily: "Metropolis",
+                  fontSize: 30,
+                  color: Colors.grey.shade800),
+              textAlign: TextAlign.center,
             ),
-          ))
+            const SizedBox(
+              height: 10,
+            ),
+            Text("ovulation phase",
+                style: TextStyle(
+                    fontFamily: "Metropolis",
+                    fontSize: 15,
+                    color: Colors.purple.shade300))
+          ],
+        ),
+      )
     ]);
   }
 }
