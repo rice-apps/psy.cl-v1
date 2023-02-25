cd093a931e4f240d67d6146265c27974bcce1822
Ovikx <ovikdas10@gmail.com>
2023-02-25 15:14:28 -0600

    add date info

--- lib/src/features/top-calendar/mini_calendar_day.dart
+++ lib/src/features/top-calendar/mini_calendar_day.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MiniCalendarDay extends StatefulWidget {
-  const MiniCalendarDay({super.key, required this.label});
+  const MiniCalendarDay(
+      {super.key, required this.month, required this.day, required this.label});
+  final int month;
+  final int day;
   final String label;
 
   @override
@@ -19,7 +22,11 @@ class _MiniCalendarDay extends State<MiniCalendarDay> {
       decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(50)),
           color: Colors.amber),
-      child: Text(widget.label),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text(widget.label), Text(widget.day.toString())],
+      ),
     );
   }
 }
--- lib/src/features/top-calendar/top_calendar.dart
+++ lib/src/features/top-calendar/top_calendar.dart
@@ -10,24 +10,19 @@ class TopCalendar extends StatefulWidget {
 }
 
 class _TopCalendar extends State<TopCalendar> {
-  int _currentDay = 1;
-  late List<GestureDetector> days = generateDays(30);
-  final PageController controller = PageController(viewportFraction: 0.3);
+  // Fetch date data
+  DateTime today = DateTime.now();
+  late final DateTime _lastDay = DateTime.utc(today.year, today.month + 1, 0);
+  late final DateTime _firstDay = DateTime.utc(today.year, today.month, 1);
+  DateTime? _selectedDay;
+  late int _currentDay = today.day;
+  late int _currentMonth = today.month;
+  late int _currentYear = today.year;
+  late List<GestureDetector> days = generateDays(_lastDay.day);
+  final PageController controller = PageController(viewportFraction: 0.2);
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   height: 75,
-    //   child: ListView.separated(
-    //     itemCount: days.length,
-    //     separatorBuilder: (BuildContext context, int index) =>
-    //         SizedBox(width: 10),
-    //     itemBuilder: (BuildContext context, int index) {
-    //       return days[index];
-    //     },
-    //     scrollDirection: Axis.horizontal,
-    //   ),
-    // );
     return Container(
         height: 75,
         child: PageView.builder(
@@ -37,7 +32,10 @@ class _TopCalendar extends State<TopCalendar> {
             _currentDay = index + 1;
           }),
           itemBuilder: (context, idx) {
-            return days[idx];
+            return Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: days[idx],
+            );
           },
         ));
   }
@@ -46,11 +44,33 @@ class _TopCalendar extends State<TopCalendar> {
   List<GestureDetector> generateDays(int numDays) {
     List<GestureDetector> days = [];
     for (int i = 0; i < numDays; i++) {
+      DateTime targetDay = DateTime(_currentYear, _currentMonth, i + 1);
+      print(targetDay);
       days.add(GestureDetector(
-        child: MiniCalendarDay(label: (i + 1).toString()),
+        child: MiniCalendarDay(
+          month: today.month,
+          day: i + 1,
+          label: intToWeekday(targetDay.weekday),
+        ),
       ));
     }
 
     return days;
+  }
+
+  /// Converts a integer 0-7 into an abbreviated weekday name
+  String intToWeekday(int num) {
+    assert(num >= 0 && num <= 7);
+    Map<int, String> weekdays = {
+      1: 'Mon',
+      2: 'Tues',
+      3: 'Wed',
+      4: 'Thurs',
+      5: 'Fri',
+      6: 'Sat',
+      7: 'Sun'
+    };
+
+    return weekdays[num]!;
   }
 }
