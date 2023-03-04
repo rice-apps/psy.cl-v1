aef8ce15acdec2502fd366094eb1a9919772422c
Ovikx <ovikdas10@gmail.com>
2023-03-04 13:33:11 -0600

    add current day highlighting

--- lib/src/features/top-calendar/mini_calendar_day.dart
+++ lib/src/features/top-calendar/mini_calendar_day.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 
 class MiniCalendarDay extends StatefulWidget {
   const MiniCalendarDay(
-      {super.key, required this.month, required this.day, required this.label});
+      {super.key,
+      required this.month,
+      required this.day,
+      required this.label,
+      required this.selected});
   final int month;
   final int day;
   final String label;
+  final bool selected;
 
   @override
   _MiniCalendarDay createState() => _MiniCalendarDay();
 }
 
 class _MiniCalendarDay extends State<MiniCalendarDay> {
-  final bool _selected = false;
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,11 +24,25 @@ class _MiniCalendarDay extends State<MiniCalendarDay> {
       width: 35,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(50)),
-          color: Colors.amber),
+          color: Colors.transparent,
+          boxShadow: widget.selected
+              ? [
+                  BoxShadow(
+                      color: Colors.purple,
+                      blurStyle: BlurStyle.outer,
+                      blurRadius: 10)
+                ]
+              : []),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text(widget.label), Text(widget.day.toString())],
+        children: [
+          Text(widget.label),
+          SizedBox(
+            height: 8,
+          ),
+          Text(widget.day.toString())
+        ],
       ),
     );
   }
--- lib/src/features/top-calendar/top_calendar.dart
+++ lib/src/features/top-calendar/top_calendar.dart
@@ -14,11 +14,11 @@ class _TopCalendar extends State<TopCalendar> {
   DateTime today = DateTime.now();
   late final DateTime _lastDay = DateTime.utc(today.year, today.month + 1, 0);
   late final DateTime _firstDay = DateTime.utc(today.year, today.month, 1);
-  DateTime? _selectedDay;
   late int _currentDay = today.day;
-  late int _currentMonth = today.month;
-  late int _currentYear = today.year;
-  late List<GestureDetector> days = generateDays(_lastDay.day);
+  late final int _currentMonth = today.month;
+  late final int _currentYear = today.year;
+
+  // Controller
   final PageController controller = PageController(viewportFraction: 0.2);
 
   @override
@@ -27,35 +27,27 @@ class _TopCalendar extends State<TopCalendar> {
         height: 75,
         child: PageView.builder(
           controller: controller,
-          itemCount: days.length,
+          itemCount: _lastDay.day,
           onPageChanged: (int index) => setState(() {
             _currentDay = index + 1;
           }),
           itemBuilder: (context, idx) {
+            DateTime targetDay = DateTime(_currentYear, _currentMonth, idx + 1);
+            MiniCalendarDay miniDay = MiniCalendarDay(
+              month: targetDay.month,
+              day: targetDay.day,
+              label: intToWeekday(targetDay.weekday),
+              selected: targetDay.day == _currentDay,
+            );
+            print("Current day: $_currentDay");
+            print("Target day: ${targetDay.day}");
+            print(targetDay.day == _currentDay);
             return Padding(
               padding: EdgeInsets.only(left: 10, right: 10),
-              child: days[idx],
+              child: miniDay,
             );
           },
         ));
-  }
-
-  /// Generates a list of [numDays] clickable days for the mini-calendar
-  List<GestureDetector> generateDays(int numDays) {
-    List<GestureDetector> days = [];
-    for (int i = 0; i < numDays; i++) {
-      DateTime targetDay = DateTime(_currentYear, _currentMonth, i + 1);
-      print(targetDay);
-      days.add(GestureDetector(
-        child: MiniCalendarDay(
-          month: today.month,
-          day: i + 1,
-          label: intToWeekday(targetDay.weekday),
-        ),
-      ));
-    }
-
-    return days;
   }
 
   /// Converts a integer 0-7 into an abbreviated weekday name
