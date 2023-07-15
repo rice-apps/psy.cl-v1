c45bee295b7350787e674c1db51f4ba0cba125c7
swaharoy <swaharoy3@gmail.com>
2023-07-15 12:03:19 -0500

    Updated grid for filter buttons

--- lib/src/features/recommendation/recommendation-page.dart
+++ lib/src/features/recommendation/recommendation-page.dart
@@ -8,105 +8,77 @@ class Recommendation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 30),
-            child: Text(
-              'What are you looking for today?',
-                  style: TextStyle(
-                    fontFamily: "Metropolis",
-                    fontSize: 25,
-                    color: Colors.grey.shade800),
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 40),
+          padding: const EdgeInsets.only(top: 15, left: 20, right: 20, bottom: 5),
+          child: Text(
+            'What are you looking for today?',
+                style: TextStyle(
+                  fontFamily: "Metropolis",
+                  fontSize: 40,
+                  color: Colors.grey.shade800),
                   textAlign: TextAlign.center,
                 ),
           ),
-          Text(
-            'Choose up to six focus areas.',
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.grey.shade400),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-          height: 298, // Adjust the height as needed
-          child: GridView.count(
-              padding: EdgeInsets.only(top: 20, left: 90, right: 90),
-
-              crossAxisSpacing: 50,
-              mainAxisSpacing: 50,
-              crossAxisCount: 3,
-              children: [
-                /*
-                buildCircleButton('🍎', 'Diet'),
-                buildCircleButton('💼', 'Work'),
-                buildCircleButton('💪', 'Fitness'),
-                buildCircleButton('🌟', 'Output'),
-                buildCircleButton('😊', 'Mood'),
-                buildCircleButton('⚡', 'Energy'),
-                */
-              ],
-            
-          ),
+        Text(
+          'Choose up to six focus areas.',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.grey.shade600),
+          textAlign: TextAlign.center,
         ),
+        SizedBox(
+          height: 360,
+          child: GridView.count(
+            crossAxisCount: 3,
+            padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+            crossAxisSpacing: 40,
+            mainAxisSpacing: 50,
+            children: [
+              //Replace containers with functional filter buttons
+              Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue),
+              ),
+
+              Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue),
+              ),
+
+              Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue),
+              ),
+
+              Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue),
+              ),
+
+              Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue),
+              )
+            ]
+          )
+        ),  
       ],
     );
     
     
   }
-
-
-//build circle button 
-Widget buildCircleButton(String emoji, String buttonText) {
-  return Column(
-    children: [
-      InkWell(
-        onTap: () {
-          // Handle button tap
-        },
-        child: Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                blurRadius: 8,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Center(
-            child: Text(
-              emoji,
-              style: TextStyle(
-                fontSize: 30, //default font is black and white
-              ),
-            ),
-          ),
-        ),
-      ),
-    //]
-  SizedBox(height: 8), // Add spacing between the button and text
-  Text(
-    buttonText,
-    style: TextStyle(
-      fontSize: 10,
-      color: Colors.grey.shade800,
-    ),
-  ),
-  ],    
-  );
-}
 }
 
-/*
-buildCircleButton('🍎'),
-                buildCircleButton('💼'),
-                buildCircleButton('💪'),
-                buildCircleButton('🌟'),
-                buildCircleButton('😊'),
-                buildCircleButton('⚡'),
-                */
